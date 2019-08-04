c44cc498a98c2bd2e7f709242ab1a2dbc71a7400
Lameaux <sergii.sizov@gmail.com>
2019-08-04 14:18:45 +0200

    express timestamp endpoint

---
+++ node-express/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/timestamp',
+    (_req, res) => res.json(
+        {
+            timestamp: new Date().getTime()
+        }
+    )
+);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
