bf41bf40e3aa7c877e797e2414b3bd793b10ad1a
Lameaux <sergii.sizov@gmail.com>
2019-08-19 19:58:21 +0200

    nodejs + express

--- node-express/server.js
+++ node-express/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const port = 3001;
 
 app.get('/timestamp',
@@ -9,5 +10,37 @@ app.get('/timestamp',
         }
     )
 );
+
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/nodejs';
+
+mongoose.connect(url, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const Schema = mongoose.Schema;
+
+const DocumentSchema = new Schema({ title: String });
+
+const DocumentModel = mongoose.model('Document', DocumentSchema );
+
+app.post('/documents', (req, res) => {
+
+    const document = new DocumentModel({
+        title: `Document ${new Date().getTime()}`
+    });
+    document.save().catch(error => { console.log(error) });
+
+    res.json(document);
+});
+
+app.get('/documents', (req, res) => {
+    DocumentModel
+        .findOne({})
+        .exec(function (err, document) {
+            if (err) console.log(err);
+            res.json(document);
+        });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
